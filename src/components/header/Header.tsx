9721bf31b3fbbeaeaf69be21ae42a910fd9e4d54
Tetiana Bukoros <tetyana.bukoros@gmail.com>
2024-10-18 00:18:57 +0300

    adapted header view to mobile

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -6,17 +6,24 @@ const Header = () => {
   return (
     <header className="header">
       <div className="header__wrapper">
-        <h1>
-          <strong>Hi, my name is <em>Tetiana</em></strong>
-        </h1>
-
-        <h2>a frontend developer</h2>
-
-        <div className="header__text">
-          <p>with passion for learning and creating</p>
+        <div className="header__text-wrapper">
+          <h1>
+            <strong>Hi, my name is <em>Tetiana</em></strong>
+          </h1>
+          <h2>a frontend developer</h2>
+          <div className="header__text">
+            <p>with passion for learning and creating</p>
+          </div>
         </div>
 
-        <a href={resume} download='Tetiana Bukoros CV Frontend' target="_blank" className="header__btn skeleton-button" rel="noreferrer">Download CV</a>
+        <a href={resume}
+          download='Tetiana Bukoros CV Frontend'
+          target="_blank"
+          className="header__btn skeleton-button"
+          rel="noreferrer"
+        >
+          Download CV
+        </a>
       </div>
     </header>
   );
